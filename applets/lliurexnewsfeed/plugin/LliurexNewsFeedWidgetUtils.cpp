60940285cbd94d55aef0a320ca220d96edbd8f3a
jrpelegrina <juapesai@hotmail.com>
2026-03-09 10:45:41 +0100

    Fixed

--- applets/lliurexnewsfeed/plugin/LliurexNewsFeedWidgetUtils.cpp
+++ applets/lliurexnewsfeed/plugin/LliurexNewsFeedWidgetUtils.cpp
@@ -12,6 +12,7 @@
 #include <QUrl>
 #include <QtConcurrent>
 #include <QMetaType>
+#include <QPointer>
 
 
 
@@ -127,30 +128,35 @@ void LliurexNewsFeedWidgetUtils::getBlogRssInfo(){
 
 void LliurexNewsFeedWidgetUtils::processBlogRssInfo(QVariantList blogRssEntries){
 
-    QtConcurrent::run([this,blogRssEntries]() {
+    QPointer<LliurexNewsFeedWidgetUtils>safeThis(this);    
+    QtConcurrent::run([safeThis,blogRssEntries]() {
+        
+        if (!safeThis){
+            return;
+        }
         QVector <LliurexNewsFeedWidgetRssItem> blogRssModel;
         bool areNews=false;
         bool firstRun=true;
         
         if (blogRssEntries.count()>0){
-            lastBlogRssUpdate=getLastRssUpdate(lastBlogUpdatePath);
+            safeThis->lastBlogRssUpdate=safeThis->getLastRssUpdate(safeThis->lastBlogUpdatePath);
             QVariantMap lastItem=blogRssEntries.first().toMap();
             QString newUpdateBlogRssDate=lastItem["pubDate"].toString();
-            blogRssModel=setDataForModel(blogRssEntries);
+            blogRssModel=safeThis->setDataForModel(blogRssEntries);
             if (!newUpdateBlogRssDate.isEmpty()){
-                if (!lastBlogRssUpdate.isEmpty()){
+                if (!safeThis->lastBlogRssUpdate.isEmpty()){
                     firstRun=false;
                     QDate newUpdate=QDate::fromString(newUpdateBlogRssDate,Qt::RFC2822Date);
-                    QDate previousDate=QDate::fromString(lastBlogRssUpdate,Qt::RFC2822Date);
+                    QDate previousDate=QDate::fromString(safeThis->lastBlogRssUpdate,Qt::RFC2822Date);
                     if (newUpdate>previousDate){
                         areNews=true;
                     }  
                 }
-                updateLastRssPath(lastBlogUpdatePath,newUpdateBlogRssDate);
+                safeThis->updateLastRssPath(safeThis->lastBlogUpdatePath,newUpdateBlogRssDate);
             }
         }
 
-        emit blogRssProcessed(blogRssModel,areNews,firstRun);
+        emit safeThis->blogRssProcessed(blogRssModel,areNews,firstRun);
     });
 }
 
